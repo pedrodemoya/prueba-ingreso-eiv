58415b5a7f1db904afe79e614db1322e6624102b
Pedro de Moya <pedrodemoya@gmail.com>
2021-11-23 20:08:25 -0300

    Correciones menores

--- src/main/java/com/eiv/pruebaingreso/repositories/PersonaRepository.java
+++ src/main/java/com/eiv/pruebaingreso/repositories/PersonaRepository.java
@@ -1,10 +1,6 @@
 package com.eiv.pruebaingreso.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.eiv.pruebaingreso.entities.Persona;
@@ -14,11 +10,5 @@ import com.eiv.pruebaingreso.entities.PersonaPK;
 //Ver tipo variable id
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, PersonaPK> {
-	
-	@Query("SELECT p FROM Persona p WHERE p.nombreApellido LIKE %:busqueda% OR l.correoElectronico LIKE %:busqueda%")
-	public List<Persona> buscarPorNombreOmail (@Param("busqueda") String busqueda);
-	
-	@Query("SELECT p FROM Persona p WHERE p.localidad LIKE %:busqueda% OR l.localidad.provincia LIKE %:busqueda%")
-	public List<Persona> buscarPorLocalidadOprovincia (@Param("busqueda") String busqueda);
 	
 }
